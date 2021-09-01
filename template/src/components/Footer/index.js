537db323ec38643b5cfb3fdd0365b581b8e6d9c9
lavender <lavender@lavenderdeMacBook-Pro.local>
2021-09-02 03:36:42 +0800

    Optimize layout

--- template/src/components/Footer/index.js
+++ template/src/components/Footer/index.js
@@ -1,20 +1,20 @@
-import React, {Component} from 'react'
-import {Image, Container} from 'semantic-ui-react';
-import './footer.css';
+import React, { Component } from 'react'
+import { Image, Container } from 'semantic-ui-react';
+import styles from './footer.module.css';
 import logo from '../../assets/polygon-logo.svg'
 
-export default class StickyLayout extends Component {
+export default class Footer extends Component {
   render() {
     return (
-    <Container>
-      <footer className="footer">
-        <div className="created">
-          Created by
-          <Image size='small' src={logo} style={{marginLeft: "10px"}}/>
-        </div>
-        <div className="copyright">Copyright © 2021 Polygon technology</div>
-      </footer>
-    </Container>
+      <Container>
+        <footer className={styles.footer}>
+          <div className={styles.created}>
+            Created by
+            <Image size='small' src={logo}/>
+          </div>
+          <div className={styles.copyright}>Copyright © 2021 Polygon technology</div>
+        </footer>
+      </Container>
     )
   }
 }
--- template/src/components/Wallet/ConnectWallet.js
+++ template/src/components/Wallet/ConnectWallet.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useWeb3React } from '@web3-react/core'
 
 import { useEagerConnect, useInactiveListener } from '../../hooks'
@@ -6,15 +6,13 @@ import connectorList, { resetWalletConnectConnector } from '../../lib/connectors
 import { Dropdown } from 'semantic-ui-react'
 
 const ConnectWallet = () => {
-  const [isConnecing, setIsConnecing] = useState(false);
-  const {activate, deactivate, active, error} = useWeb3React();
+  const { activate, deactivate, active, error } = useWeb3React();
 
   const triedEager = useEagerConnect();
 
   useInactiveListener(!triedEager);
 
   const handleClick = (connectorName) => () => {
-    setIsConnecing(true);
     activate(connectorList[connectorName]);
   };
 
@@ -23,44 +21,37 @@ const ConnectWallet = () => {
   };
 
   const handleRetry = () => {
-    setIsConnecing(false);
     resetWalletConnectConnector(connectorList['WalletConnect']);
     deactivate();
   };
 
-  useEffect(() => {
-    if (active) {
-      setIsConnecing(false);
-    }
-  }, [active]);
-
   return (
-  <Dropdown text='Wallet' pointing className='link item'>
+    <Dropdown text='Wallet' pointing className='link item'>
 
 
-    {(() => {
-      if (active) {
-        return <Dropdown.Menu>
-          <Dropdown.Item className="button-disconnect" onClick={handleDisconnect}>
-            Disconnect Wallet
-          </Dropdown.Item>
-        </Dropdown.Menu>
-      } else {
-        if (!error) {
+      {(() => {
+        if (active) {
           return <Dropdown.Menu>
-            <Dropdown.Item onClick={handleClick('MetaMask')}>MetaMask</Dropdown.Item>
-            <Dropdown.Item onClick={handleClick('WalletConnect')}>WalletConnect</Dropdown.Item>
-            <Dropdown.Item onClick={handleClick('WalletLink')}>WalletLink</Dropdown.Item>
+            <Dropdown.Item className="button-disconnect" onClick={handleDisconnect}>
+              Disconnect Wallet
+            </Dropdown.Item>
           </Dropdown.Menu>
         } else {
-          return <Dropdown.Menu>
-            <Dropdown.Item onClick={handleRetry}>Retry</Dropdown.Item>
-          </Dropdown.Menu>
+          if (!error) {
+            return <Dropdown.Menu>
+              <Dropdown.Item onClick={handleClick('MetaMask')}>MetaMask</Dropdown.Item>
+              <Dropdown.Item onClick={handleClick('WalletConnect')}>WalletConnect</Dropdown.Item>
+              <Dropdown.Item onClick={handleClick('WalletLink')}>WalletLink</Dropdown.Item>
+            </Dropdown.Menu>
+          } else {
+            return <Dropdown.Menu>
+              <Dropdown.Item onClick={handleRetry}>Retry</Dropdown.Item>
+            </Dropdown.Menu>
+          }
         }
-      }
-    })()}
+      })()}
 
-  </Dropdown>
+    </Dropdown>
   );
 };
 
